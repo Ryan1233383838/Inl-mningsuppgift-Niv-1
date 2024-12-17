9c412b9487bba32d239cb9a9863163184fc418fd
ryhu06001 <ryhu06001@E-PW05CGRC>
2024-12-17 13:50:55 +0100

    Add project files.

---
+++ Inlämningsuppgift-Nivå 1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Inlämningsuppgift_Nivå_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            for (int i = 1; i <= 10; i++) 
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
